01216c5caf9598d3cfc7e5cbd55f8092661b0e93
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2021-06-30 17:09:10 -0300

    remove

--- src/config/auth.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  jwt: {
-    secret: '3649ed4db4b8c5cfef7ccb1c762db170',
-    expiresIn: '1h',
-  },
-};
-  
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import candidateTypeRouter from './candidate.routes';
-
-const routes = Router();
- 
-routes.use('/candidate', candidateTypeRouter);
-
-export default routes;
